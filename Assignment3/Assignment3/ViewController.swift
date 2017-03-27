39b8c721dedc8a9a75288a3731777ca04a8aa536
Janak Patel <janakpatel@gmail.com>
2017-03-27 01:03:05 -0500

    Added Audio Track Vance Lane Texas Two Step

--- Assignment3/Assignment3/ViewController.swift
+++ Assignment3/Assignment3/ViewController.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
     @IBOutlet weak var myStepButton: UIButton!  // ctrl dragged Step button here
     @IBOutlet weak var gridView: GridView!  // ctrl dragged Grid View here
+    var player:AVAudioPlayer = AVAudioPlayer()
     
     @IBAction func myStepButtonAction(_ sender: Any) {
+        // Plays audio file.
+        audioPlayer.play()
+        
+        // Updates grid state to its next state.
         gridView.myGrid = gridView.myGrid.next()  // Updates myGrid to its next state.
         gridView.setNeedsDisplay()  // Refreshes display of Grid View.
     }
@@ -21,13 +28,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "VanceLaneTTS", ofType: "m4a")!))
+            audioPlayer.prepareToPlay()
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
